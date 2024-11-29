7c739c7d221e2d3c1d095189c9019ef2e983cd97
6509650419techit <techit.cha@dome.tu.ac.th>
2024-11-30 01:40:32 +0700

    fix

--- user-data.sh
+++ user-data.sh
@@ -10,23 +10,7 @@ sudo systemctl enable docker
 # Add the ec2-user to the Docker group (so you can run Docker commands without sudo)
 sudo usermod -aG docker ec2-user
 
-# Pull the Docker image and run the container
-# Specify the Docker image and container name
-IMAGE_NAME="fieldrock/cs360_frontend_image_test:latest"
-CONTAINER_NAME="test-frontend-container"
 
-# Pull the Docker image
-echo "Pulling Docker image: $IMAGE_NAME"
-sudo docker pull $IMAGE_NAME
+sudo docker pull fieldrock/cs360_frontend_image_test:latest
+docker run -d -p 3000:3000 --name cs360_frontend_container fieldrock/cs360_frontend_image_test:latest
 
-# Check if the image was pulled successfully
-if [ $? -eq 0 ]; then
-  echo "Docker image pulled successfully."
-
-  # Run the container
-  echo "Running the Docker container..."
-  sudo docker run -d -p 3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
-else
-  echo "Failed to pull Docker image. Exiting."
-  exit 1
-fi
